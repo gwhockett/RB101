52d2b986a7435694276a86d4105db352ed467dc1
George Hockett <gwhockett@gmail.com>
2022-04-28 12:41:04 -0400

    intitiate_deck, prompt, not_ace_numeric_value, not_aces_total, aces_total,

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,21 +1,5 @@
-# def initiate_deck
-#   suit = %w(H D S C)
-#   cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-#   deck = []
-#   until cards.size == 0
-#     suit.map do |card|
-#       arr = []
-#       arr << card
-#       deck << arr
-#     end
-#     a = cards.shift
-#     deck.each do |num|
-#       num << a if num.size < 2
-#     end
-#   end
-#   deck
-# end
 
+require "pry"
 def initiate_deck
   suit = %w(H D S C)
   cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
@@ -33,4 +17,65 @@ def initiate_deck
   deck
 end
 
-p initiate_deck
+def display_cards()
+  puts "-----Dealer-----"
+  puts "---#{dealt(card)}-|-#{dealt(card)}---"
+  puts "***************"
+  puts "***************"
+  puts "------Player------"
+  puts "---#{dealt(card)}-|-#{dealt(card)}---"
+end
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+deck = initiate_deck.shuffle
+
+def not_ace_numeric_value(card)
+  value = 0
+  if card[1].to_i != 0
+    value += card[1].to_i
+  elsif card[1].to_i == 0 && card[1] != "A"
+    value += 10
+  elsif card[1] == "A"
+    "A"
+  end
+end
+
+delt = []
+
+delt << not_ace_numeric_value(deck.pop)
+delt << not_ace_numeric_value(deck.pop)
+delt << not_ace_numeric_value(deck.pop)
+delt << not_ace_numeric_value(deck.pop)
+
+p delt
+
+total = 0
+
+def not_aces_total(cards)
+  not_aces = []
+  cards.select do |card|
+    not_aces << card if card.is_a?(Integer)
+  end
+  not_aces.sum
+end
+
+total += not_aces_total(delt)
+
+def aces_total(cards, current_total)
+  aces_amount = 0
+  cards.each do |card|
+    if current_total + 11 > 21 && card == "A"
+      aces_amount += 1
+    elsif card == "A"
+      aces_amount += 11
+    end
+  end
+  aces_amount
+end
+
+p aces_total(delt, total)
+p total
+p total += aces_total(delt, total)
