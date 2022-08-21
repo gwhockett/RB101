803dec077d3e17b4c798294ef3c49c77c4659779
George Hockett <gwhockett@gmail.com>
2022-08-21 13:47:21 -0400

    RB101 medium_2 done!

--- RB101_109_small_problems/medium_2.rb
+++ RB101_109_small_problems/medium_2.rb
@@ -141,12 +141,93 @@ p triangle(120, 50, 10) == :obtuse
 p triangle(0, 90, 90) == :invalid
 p triangle(50, 50, 50) == :invalid
 
-ex. 7 
-=end
-def friday_13th(year)
+ex. 7 Unlucky Days
 
+require 'date'
+def friday_13th(year)
+  fridays = 0
+  (1..12).to_a.each do |m|
+    fridays += 1 if Date.new(year,m,13).friday?
+  end
+  fridays
 end
 
-p friday_13th(2015) == 3
-p friday_13th(1986) == 1
-p friday_13th(2019) == 2
+p friday_13th(2015) #== 3
+p friday_13th(1986) #== 1
+p friday_13th(2019) #== 2
+
+ex. 8 Next Featured Number Higher than a Given Value
+
+def featured(num)
+  featured = num
+  loop do
+    featured += 1
+    if featured % 7 == 0 &&
+       featured.to_s.size == featured.to_s.chars.uniq.join.size &&
+       featured.odd?
+    then
+      return featured
+    end 
+    if featured >= 9_999_999_999
+      return "There is no possible number that fulfills those requirements"
+    end
+  end
+end
+
+p featured(12) == 21
+p featured(20) == 21
+p featured(21) == 35
+p featured(997) == 1029
+p featured(1029) == 1043
+p featured(999_999) == 1_023_547
+p featured(999_999_987) == 1_023_456_987
+
+p featured(9_999_999_999) # -> There is no possible number that fulfills those requirements
+
+ex. 9 Bubble Sort
+
+def bubble_sort!(ary)
+  n = 0
+  no_swap = 0
+  loop do
+    if ary[n] > ary[n + 1] && ary[n] != ary[-1]
+      ary[n], ary[n + 1] = ary[n + 1], ary[n]
+    elsif ary[n] <= ary[n + 1]
+      no_swap += 1
+    end
+    break if no_swap == ary.size - 1
+    n += 1
+    n = 0 if n + 1 == ary.size
+    no_swap = 0 if n == 0
+  end
+  ary
+end
+
+array = [5, 3, 3]
+p bubble_sort!(array)
+p array == [3, 3, 5]
+
+array = [6, 2, 7, 1, 4, 2]
+p bubble_sort!(array)
+p array == [1, 2, 2, 4, 6, 7]
+
+array = %w(Sue Pete Pete Alice Tyler Rachel Kim Bonnie)
+p bubble_sort!(array)
+p array == %w(Alice Bonnie Kim Pete Pete Rachel Sue Tyler)
+
+ex. 10 Sum Square - Square Sum
+=end
+def sum_square_difference(num)
+  sum_square = (1..num).to_a.sum ** 2
+  square_sum = (1..num).to_a.map do |i|
+    i**2
+  end.sum
+
+  sum_square - square_sum
+end
+
+p sum_square_difference(3) #== 22
+   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+p sum_square_difference(10) #== 2640
+p sum_square_difference(1) #== 0
+p sum_square_difference(100) #== 25164150
