76456195e19a22e68bc4790618a8e8c4d5df0d94
George Hockett <gwhockett@gmail.com>
2022-09-07 13:37:20 -0400

    RB101 small_problems advanced_1 one more to go

--- RB101_109_small_problems/advanced_1.rb
+++ RB101_109_small_problems/advanced_1.rb
@@ -261,8 +261,7 @@ p merge([], [1, 4, 5]) #== [1, 4, 5]
 p merge([1, 4, 5], []) #== [1, 4, 5]
 
 ex. 8 Merge Sort
-=end
-require 'pry'
+
 def merge(l, s)  
   result = []
   l_index = 0
@@ -294,3 +293,6 @@ p merge_sort([5, 3]) #== [3, 5]
 p merge_sort([6, 2, 7, 1, 4]) #== [1, 2, 4, 6, 7]
 p merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) #== %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
 p merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) #== [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
+
+ex. 9 Egyption Fractions 
+=end
